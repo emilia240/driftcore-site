f19b6ae274d5002366e6b0e6639d61d8e3b21965
emilia240 <emisco01@easv365.dk>
2026-05-15 02:25:44 +0200

    style: Add custom theme, fonts, and fluid typography

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
 
 <template>
   <div id="app">
+    <!-- Test element -->
+    <div class="bg-navy text-cream m-2 p-8 rounded-xl">
+      <h1 class="font-heading text-h1 mb-4">
+        DriftCore
+      </h1>
+      <p class="font-body text-body">
+        Testing custom theme tokens!
+      </p>
+    </div>
+
+
     <router-view />
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#F9F9F9',
+        navy: '#213345',
+        'blue-light': '#E4ECFC',
+        'blue-muted': '#4A5D87',
+        cyan: '#54C9EB',
+      },
+      fontFamily: {
+        heading: ['Bricolage Grotesque', 'sans-serif'],
+        body: ['Inter Tight', 'sans-serif'],
+      },
+      fontSize: {
+        'body': 'clamp(1rem, 0.95rem + 0.25vw, 1.125rem)',
+        'h5': 'clamp(1.125rem, 1.05rem + 0.375vw, 1.375rem)',
+        'h4': 'clamp(1.25rem, 1.15rem + 0.5vw, 1.625rem)',
+        'h3': 'clamp(1.5rem, 1.35rem + 0.75vw, 2rem)',
+        'h2': 'clamp(1.875rem, 1.65rem + 1.125vw, 2.625rem)',
+        'h1': 'clamp(2.25rem, 1.85rem + 2vw, 3.75rem)',
+      },
+      borderRadius: {
+        'xl': '25px',
+      }
+    },
+  },
+}
